16f84e77da12aef2553dce31cad13380e9fc28ba
tsukurobo <tsukurobo@example.com>
2023-11-09 09:12:50 +0000

    誤字修正

--- src/joy_control/src/TwoWheelDrive.cpp
+++ src/joy_control/src/TwoWheelDrive.cpp
@@ -59,7 +59,7 @@ class TwoWDSpeedConverter{
 		// 以下をターミナルに打ち込んで表示
 		/* rosrun rqt_console rqt_console */
 		ROS_DEBUG("target_ang_velL:%f", ang_vel[0]);
-		ROS_DEBUG("target_ang_velL:%f", ang_vel[1]);
+		ROS_DEBUG("target_ang_velR:%f", ang_vel[1]);
 		
 		// 角速度を[rad/s]からエンコーダの[count/s]に変換
 		custom_msgs::TwoWDAngVel cmd_w;
