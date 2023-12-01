b7473b47081f41f14022b166dd7cea7309548682
Neko2234 <99799346+Neko2234@users.noreply.github.com>
2023-12-02 01:20:51 +0900

    debugging arm

--- src/Arduino/TwoWD/TwoWD.ino
+++ src/Arduino/TwoWD/TwoWD.ino
@@ -3,16 +3,16 @@
 #include "PID.h"
 #include "Cubic.controller.h"
 
-#include <ros.h>
-#include <std_msgs/Float32MultiArray.h>
-#include "custom_msgs/TwoWDAngVel.h"
-// #include "custom_msgs/WheelEnc.h"
-#include "custom_msgs/ArmVel.h"
+// #include <ros.h>
+// #include <std_msgs/Float32MultiArray.h>
+// #include "custom_msgs/TwoWDAngVel.h"
+// // #include "custom_msgs/WheelEnc.h"
+// #include "custom_msgs/ArmVel.h"
 
 #define LEFT_MOTOR 0
 #define RIGHT_MOTOR 1
 
-int arm_duty = 0;
+float arm_duty = 0;
 int arm_enc_num = 2;
 float cmd_ang_vel[2] = {0.0, 0.0};
 float arm_vel = 450;
@@ -25,42 +25,42 @@ enum class Arm_queue
 	Close,
 	Standby
 };
-float openArmEnc = 100;
-float closeArmEnc = 10;
+float openArmEnc = 0;
+float closeArmEnc = 1000;
 
-std_msgs::Float32MultiArray gainL_msg; // PIDゲインを受け取るための配列
-std_msgs::Float32MultiArray gainR_msg;
-custom_msgs::TwoWDAngVel wheel_vel;
+// std_msgs::Float32MultiArray gainL_msg; // PIDゲインを受け取るための配列
+// std_msgs::Float32MultiArray gainR_msg;
+// custom_msgs::TwoWDAngVel wheel_vel;
 
 // エンコーダのCPR
 int enc_cpr = 1028 * 4;
 
-ros::NodeHandle nh;
+//ros::NodeHandle nh;
 
-ros::Publisher enc_pub("wheelVel", &wheel_vel);
+//ros::Publisher enc_pub("wheelVel", &wheel_vel);
 // ros::Publisher gainL_pub("Arduino/gainL", &gainL_msg);
 // ros::Publisher gainR_pub("Arduino/gainR", &gainR_msg);
 
 // 単位は[count/sec]
-void angVelCb(const custom_msgs::TwoWDAngVel &ang_vel_msg)
-{
-	// ROSから来るのが100オーダーの値で、速度PIDの目標速度のオーダーが10くらいなので10で割ってる
-	// ロボットにとって前方向への移動のとき左タイヤは正方向、右タイヤは負方向に回転するので符号をつけて補正している。
-	cmd_ang_vel[LEFT_MOTOR] = ang_vel_msg.L / 10;
-	cmd_ang_vel[RIGHT_MOTOR] = -ang_vel_msg.R / 10;
-}
+// void angVelCb(const custom_msgs::TwoWDAngVel &ang_vel_msg)
+// {
+// 	// ROSから来るのが100オーダーの値で、速度PIDの目標速度のオーダーが10くらいなので10で割ってる
+// 	// ロボットにとって前方向への移動のとき左タイヤは正方向、右タイヤは負方向に回転するので符号をつけて補正している。
+// 	cmd_ang_vel[LEFT_MOTOR] = ang_vel_msg.L / 10;
+// 	cmd_ang_vel[RIGHT_MOTOR] = -ang_vel_msg.R / 10;
+// }
 
-ros::Subscriber<custom_msgs::TwoWDAngVel> ang_vel_sub("arduino/cmd_w", &angVelCb);
+// ros::Subscriber<custom_msgs::TwoWDAngVel> ang_vel_sub("arduino/cmd_w", &angVelCb);
 
-// 単位は[count/sec]
-void armVelCb(const custom_msgs::ArmVel &arm_vel_msg)
-{
-	// ROSから来るのが100オーダーの値で、速度PIDの目標速度のオーダーが10くらいなので10で割ってる
-	// ロボットにとって前方向への移動のとき左タイヤは正方向、右タイヤは負方向に回転するので符号をつけて補正している。
-	// arm_vel = arm_vel_msg.vel * 20 * 250;
-}
+// // 単位は[count/sec]
+// void armVelCb(const custom_msgs::ArmVel &arm_vel_msg)
+// {
+// 	// ROSから来るのが100オーダーの値で、速度PIDの目標速度のオーダーが10くらいなので10で割ってる
+// 	// ロボットにとって前方向への移動のとき左タイヤは正方向、右タイヤは負方向に回転するので符号をつけて補正している。
+// 	// arm_vel = arm_vel_msg.vel * 20 * 250;
+// }
 
-ros::Subscriber<custom_msgs::ArmVel> arm_vel_sub("twoWD/arm_vel", &armVelCb);
+// ros::Subscriber<custom_msgs::ArmVel> arm_vel_sub("twoWD/arm_vel", &armVelCb);
 
 void setup()
 {
@@ -70,10 +70,10 @@ void setup()
 	// nh.getHardware()->setBaud(80000000);
 	// nh.initNode();
 
-	gainL_msg.data_length = 3;
-	gainL_msg.data = (float *)malloc(sizeof(float) * 3);
-	gainR_msg.data_length = 3;
-	gainR_msg.data = (float *)malloc(sizeof(float) * 3);
+	// gainL_msg.data_length = 3;
+	// gainL_msg.data = (float *)malloc(sizeof(float) * 3);
+	// gainR_msg.data_length = 3;
+	// gainR_msg.data = (float *)malloc(sizeof(float) * 3);
 
 	// ROSとつながるまで待つ
 	// while (!nh.connected())
@@ -82,25 +82,25 @@ void setup()
 	// 	delay(10);
 	// }
 
-	if (!nh.getParam("/arduino/gainL", gainL_msg.data, 3))
-	{
-		// default values
-		gainL_msg.data[0] = 7.0; // P
-		gainL_msg.data[1] = 1.0; // I
-		gainL_msg.data[2] = 0.3; // D
-	}
-	if (!nh.getParam("/arduino/gainR", gainR_msg.data, 3))
-	{
-		// default values
-		gainR_msg.data[0] = 7.0; // P
-		gainR_msg.data[1] = 1.0; // I
-		gainR_msg.data[2] = 0.3; // D
-	}
-	nh.getParam("/twoWD/enc_cpr", &enc_cpr);
+	// if (!nh.getParam("/arduino/gainL", gainL_msg.data, 3))
+	// {
+	// 	// default values
+	// 	gainL_msg.data[0] = 7.0; // P
+	// 	gainL_msg.data[1] = 1.0; // I
+	// 	gainL_msg.data[2] = 0.3; // D
+	// }
+	// if (!nh.getParam("/arduino/gainR", gainR_msg.data, 3))
+	// {
+	// 	// default values
+	// 	gainR_msg.data[0] = 7.0; // P
+	// 	gainR_msg.data[1] = 1.0; // I
+	// 	gainR_msg.data[2] = 0.3; // D
+	// }
+	// nh.getParam("/twoWD/enc_cpr", &enc_cpr);
 
-	nh.subscribe(ang_vel_sub);
-	nh.subscribe(arm_vel_sub);
-	nh.advertise(enc_pub);
+	// nh.subscribe(ang_vel_sub);
+	// nh.subscribe(arm_vel_sub);
+	// nh.advertise(enc_pub);
 	// nh.advertise(gainL_pub);
 	// nh.advertise(gainR_pub);
 	pinMode(24, OUTPUT);
@@ -111,32 +111,37 @@ void setup()
 
 void loop()
 {
-	nh.spinOnce();
+	// nh.spinOnce();
 	static bool stopFlag = false;
 
 	if (Serial.available() > 0)
 	{
-		arm_open = !arm_open;
+		String value = Serial.readStringUntil('\n');
+    if(value == "1"){
+      arm_open = true;
+    }else if(value == "0"){
+      arm_open = false;
+    }
 	}
 
 	//  nh.getParam("/Arduino/gainL", gainL_msg.data, 3);
 	//  nh.getParam("/Arduino/gainR", gainR_msg.data, 3);
 
-	static Cubic_controller::Velocity_PID velocityPID[] = {
-		{0, 0, Cubic_controller::encoderType::inc, enc_cpr, 0.7, 0.5, gainL_msg.data[0], gainL_msg.data[1], gainL_msg.data[2], 0.0, false, false}, // L
-		{1, 1, Cubic_controller::encoderType::inc, enc_cpr, 0.7, 0.5, gainR_msg.data[0], gainR_msg.data[1], gainR_msg.data[2], 0.0, false, false}, // R
-	};
+	// static Cubic_controller::Velocity_PID velocityPID[] = {
+	// 	{0, 0, Cubic_controller::encoderType::inc, enc_cpr, 0.7, 0.5, gainL_msg.data[0], gainL_msg.data[1], gainL_msg.data[2], 0.0, false, false}, // L
+	// 	{1, 1, Cubic_controller::encoderType::inc, enc_cpr, 0.7, 0.5, gainR_msg.data[0], gainR_msg.data[1], gainR_msg.data[2], 0.0, false, false}, // R
+	// };
 
-	if (cmd_ang_vel[LEFT_MOTOR] == 0 && cmd_ang_vel[RIGHT_MOTOR] == 0)
-	{
-		stopFlag = true;
-	}
-	else
-	{
-		stopFlag = false;
-		velocityPID[LEFT_MOTOR].setTarget(cmd_ang_vel[LEFT_MOTOR]);
-		velocityPID[RIGHT_MOTOR].setTarget(cmd_ang_vel[RIGHT_MOTOR]);
-	}
+	// if (cmd_ang_vel[LEFT_MOTOR] == 0 && cmd_ang_vel[RIGHT_MOTOR] == 0)
+	// {
+	// 	stopFlag = true;
+	// }
+	// else
+	// {
+	// 	stopFlag = false;
+	// 	velocityPID[LEFT_MOTOR].setTarget(cmd_ang_vel[LEFT_MOTOR]);
+	// 	velocityPID[RIGHT_MOTOR].setTarget(cmd_ang_vel[RIGHT_MOTOR]);
+	// }
 
 	// wheel_enc.L = velocityPID[LEFT_MOTOR].readEncoder();  // - Inc_enc::get_diff(LEFT_MOTOR);
 	// wheel_enc.R = velocityPID[RIGHT_MOTOR].readEncoder(); // - Inc_enc::get_diff(RIGHT_MOTOR);
@@ -152,42 +157,42 @@ void loop()
 		if (stop_log_trigger)
 		{
 			// Serial.println("stopping...");
-			nh.loginfo("Stopping...");
+			// nh.loginfo("Stopping...");
 			stop_log_trigger = false;
 		}
 
-		velocityPID[LEFT_MOTOR].reset();
-		velocityPID[RIGHT_MOTOR].reset();
+		// velocityPID[LEFT_MOTOR].reset();
+		// velocityPID[RIGHT_MOTOR].reset();
 
 		for (int i = 0; i < 8; i++)
 		{
 			DC_motor::put(i, 0);
 		}
-		wheel_vel.L = 0;
-		wheel_vel.R = 0;
+		// wheel_vel.L = 0;
+		// wheel_vel.R = 0;
 	}
 	else
 	{
 		if (!stop_log_trigger)
 		{
-			nh.loginfo("Culculating");
+			// nh.loginfo("Culculating");
 			stop_log_trigger = true;
 		}
-		wheel_vel.L = velocityPID[LEFT_MOTOR].compute();
-		wheel_vel.R = -velocityPID[RIGHT_MOTOR].compute();
+		// wheel_vel.L = velocityPID[LEFT_MOTOR].compute();
+		// wheel_vel.R = -velocityPID[RIGHT_MOTOR].compute();
 	}
 
-	arm_enc = Inc_enc::get(arm_enc_num);
+	//arm_enc = Inc_enc::get(arm_enc_num);
 	float arm_accel = pow(1 - pow(2 * arm_enc / (closeArmEnc + openArmEnc), 2), 0.5);
-	if (arm_enc > closeArmEnc && arm_open == false)
+	if (arm_enc < closeArmEnc && arm_open == false)
 	{
-		arm_duty = -1 * arm_accel * arm_vel;
+		arm_duty = arm_accel * arm_vel;
 		// digitalWrite(23,LOW);
 		// digitalWrite(24,HIGH);
 	}
-	else if (arm_enc < openArmEnc && arm_open == true)
+	else if (arm_enc > openArmEnc && arm_open == true)
 	{
-		arm_duty = arm_accel * arm_vel;
+		arm_duty = -1 * arm_accel * arm_vel;
 		// digitalWrite(23,HIGH);
 		// digitalWrite(24,LOW);
 	}
@@ -197,12 +202,19 @@ void loop()
 		// digitalWrite(24,LOW);
 		arm_duty = 0;
 	}
+
+  arm_enc =arm_enc + 0.01*arm_duty;
 	DC_motor::put(2, arm_duty);
+  Serial.print(arm_open);
+  Serial.print(",");
+  Serial.print(arm_duty);
+  Serial.print(",");
+  Serial.println(arm_enc);
 
 	// wheel_vel.L = velocityPID[LEFT_MOTOR].encoderToAngle(velocityPID[LEFT_MOTOR].readEncoder()) * radius;
 	// wheel_vel.R = velocityPID[RIGHT_MOTOR].readEncoder();
 
-	enc_pub.publish(&wheel_vel);
+	// enc_pub.publish(&wheel_vel);
 	//  gainL_pub.publish(&gainL_msg);
 	//  gainR_pub.publish(&gainR_msg);
 
