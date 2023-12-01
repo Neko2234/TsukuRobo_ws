9013b2ae0b00ec21ae8110a025bdd834f94f17c2
Neko2234 <99799346+Neko2234@users.noreply.github.com>
2023-12-01 19:10:06 +0900

    move_baseのバグを修正

--- src/navi/src/joy_twist_publisher.cpp
+++ src/navi/src/joy_twist_publisher.cpp
@@ -13,6 +13,16 @@ private:
 	ros::Subscriber joy_sub_;
 	ros::Timer timer_;
 	sensor_msgs::Joy last_joy_;
+	// ジョイコンの割り当て(yamlファイルから設定)
+	int assign_x = 1;
+	int assign_y = 0;
+	int assign_z = 2;
+	int safe_button = 7;		 // このボタンを押しながらじゃないと動かない
+	int joy_controll_button = 8; // このボタンを押すとジョイコンで動く。押さないとmove_baseで動く
+	// /* 十字ボタンで直進と回転
+	int assign_x_straight = 7;
+	int assign_z_rotate = 6;
+	// */
 
 public:
 	TwistPublisher() : nh_(), pnh_("~")
@@ -21,6 +31,13 @@ public:
 		safe_signal_pub_ = nh_.advertise<std_msgs::Bool>("safe_signal", 1);
 		joy_sub_ = nh_.subscribe("joy", 10, &TwistPublisher::joyCallback, this);
 		timer_ = nh_.createTimer(ros::Duration(0.1), &TwistPublisher::timerCallback, this);
+		pnh_.getParamCached("/joy/assign_x", assign_x);
+		pnh_.getParamCached("/joy/assign_y", assign_y);
+		pnh_.getParamCached("/joy/assign_z", assign_z);
+		pnh_.getParamCached("/joy/safe_button", safe_button);
+		pnh_.getParamCached("/joy/joy_controll_button", joy_controll_button);
+		pnh_.getParamCached("/joy/assign_x_straight", assign_x_straight);
+		pnh_.getParamCached("/joy/assign_z_rotate", assign_z_rotate);
 	}
 
 	void joyCallback(const sensor_msgs::Joy &joy_msg)
@@ -30,59 +47,47 @@ public:
 
 	void timerCallback(const ros::TimerEvent &e)
 	{
-		// ジョイコンの割り当て(launchファイルから設定)
-		int assign_x = 1;
-		int assign_y = 0;
-		int assign_z = 2;
-		int safe_button = 7;
-		pnh_.getParam("/joy/assign_x", assign_x);
-		pnh_.getParam("/joy/assign_y", assign_y);
-		pnh_.getParam("/joy/assign_z", assign_z);
-		pnh_.getParam("/joy/safe_button", safe_button);
-
-		// /* 差動二輪専用の処理 **十字ボタンで直進と回転**
-		int assign_x_straight = 7;
-		int assign_z_rotate = 6;
-		pnh_.getParam("/joy/assign_x_straight", assign_x_straight);
-		pnh_.getParam("/joy/assign_z_rotate", assign_z_rotate);
-		// */
-
-		// 最大速度の設定(launchファイルから設定)
+		// 最大速度の設定
 		float max_x = 1.0;
 		float max_y = 1.0;
 		float max_z = 1.0;
-		pnh_.getParam("/joy/max_x", max_x);
-		pnh_.getParam("/joy/max_y", max_y);
-		pnh_.getParam("/joy/max_z", max_z);
+		pnh_.getParamCached("/joy/max_x", max_x);
+		pnh_.getParamCached("/joy/max_y", max_y);
+		pnh_.getParamCached("/joy/max_z", max_z);
 
 		geometry_msgs::Twist cmd_vel;
 
-		/* joyの入力からcmd_velを計算 */
-		// /* 差動二輪専用の処理
-		if (0 <= assign_x_straight && assign_x_straight < last_joy_.axes.size())
+		/* joy_controll_buttonが押されているときだけcmd_velをpublish */
+		if (last_joy_.buttons[joy_controll_button] == 1)
 		{
-			cmd_vel.linear.x = max_x * last_joy_.axes[assign_x_straight];
+			/* joyの入力からcmd_velを計算 */
+			// /* 十字ボタンで直進と回転
+			if (0 <= assign_x_straight && assign_x_straight < last_joy_.axes.size())
+			{
+				cmd_vel.linear.x = max_x * last_joy_.axes[assign_x_straight];
+			}
+			if (0 <= assign_z_rotate && assign_z_rotate < last_joy_.axes.size())
+			{
+				cmd_vel.angular.z = max_z * last_joy_.axes[assign_z_rotate];
+			}
+			// */
+			if (0 <= assign_x && assign_x < last_joy_.axes.size())
+			{
+				if (cmd_vel.linear.x == 0)
+					cmd_vel.linear.x = max_x * last_joy_.axes[assign_x];
+			}
+			if (0 <= assign_y && assign_y < last_joy_.axes.size())
+			{
+				cmd_vel.linear.y = max_y * last_joy_.axes[assign_y];
+			}
+			if (0 <= assign_z && assign_z < last_joy_.axes.size())
+			{
+				if (cmd_vel.angular.z == 0)
+					cmd_vel.angular.z = max_z * last_joy_.axes[assign_z];
+			}
+			/* cmd_velをpublish */
+			cmd_pub_.publish(cmd_vel);
 		}
-		if (0 <= assign_z_rotate && assign_z_rotate < last_joy_.axes.size())
-		{
-			cmd_vel.angular.z = max_z * last_joy_.axes[assign_z_rotate];
-		}
-		// */
-		if (0 <= assign_x && assign_x < last_joy_.axes.size())
-		{
-			if (cmd_vel.linear.x == 0)
-				cmd_vel.linear.x = max_x * last_joy_.axes[assign_x];
-		}
-		if (0 <= assign_y && assign_y < last_joy_.axes.size())
-		{
-			cmd_vel.linear.y = max_y * last_joy_.axes[assign_y];
-		}
-		if (0 <= assign_z && assign_z < last_joy_.axes.size())
-		{
-			if (cmd_vel.angular.z == 0)
-				cmd_vel.angular.z = max_z * last_joy_.axes[assign_z];
-		}
-		cmd_pub_.publish(cmd_vel);
 
 		/* 安全ボタンの処理 */
 		std_msgs::Bool safe_signal;
